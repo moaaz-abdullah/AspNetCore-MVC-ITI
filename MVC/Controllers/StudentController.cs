9c9200ebd7ba2840a71e3044864b92cc16605f60
moaaz-abdullah <moaaz200026@gmail.com>
2026-09-02 12:46:16 +0300

    Add basic MVC structure for student listing and details
    Implemented StudentController with actions to show all students and individual student details. Added Student and StudentBL classes for data modeling and business logic. Created ShowAll and ShowById views for displaying student information. Included sample images for student profiles. Added a placeholder TestClass.

---
+++ MVC/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult ShowAll()
+        {
+            StudentBL studentBL = new StudentBL();
+            List<Student> studentsLM = studentBL.GetAll();
+            return View("ShowAll", studentsLM);
+        }
+
+        public IActionResult ShowById(int id)
+        {
+            StudentBL studentBL = new StudentBL();
+            Student studentLM = studentBL.GetById(id);
+            return View("ShowById", studentLM);
+        }
+    }
+}
---
+++ MVC/Models/StudentBL.cs
@@ -0,0 +1,25 @@
+﻿namespace MVC.Models
+{
+    public class StudentBL
+    {
+        private List<Student> students;
+
+        public StudentBL()
+        {
+            students = new List<Student>();
+            students.Add(new Student() { Id = 1, Name = "Moaaz", ImageUrl = "batman.webp" });
+            students.Add(new Student() { Id = 2, Name = "Fox", ImageUrl = "superman.webp" });
+        }
+
+        public List<Student> GetAll()
+        {
+            return students;
+        }
+
+        public Student GetById(int id)
+        {
+            return students.FirstOrDefault(student => student.Id == id);
+        }
+
+    }
+}
