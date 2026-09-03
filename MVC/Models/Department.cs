097ce74fe1bafeef5a37f5ffbc1da8e1a06a04c3
moaaz-abdullah <moaaz200026@gmail.com>
2026-09-03 13:26:01 +0300

    Add Department and Employee models, EF Core dependencies
    Added Entity Framework Core SQL Server and Tools packages to project.
    Created Department and Employee model classes with navigation properties for database integration.

---
+++ MVC/Models/Department.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC.Models
+{
+    public class Department
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string? ManagerName { get; set; }
+        public List<Employee>? Employees { get; set; }
+    }
+}
---
+++ MVC/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVC.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public int Salary { get; set; }
+
+        public string JobTitle { get; set; }
+
+        public string ImgURL { get; set; }
+
+        public string Address { get; set; }
+
+        [ForeignKey("Department")]
+        public int DepartmentID { get; set; }
+
+        public Department Department { get; set; }
+    }
+}
