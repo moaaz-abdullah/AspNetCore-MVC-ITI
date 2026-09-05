6994026bbb3b7460842b710230d65b905107e954
moaaz-abdullah <moaaz200026@gmail.com>
2026-09-05 12:37:02 +0300

    Add Employee details view and controller action
    Implemented EmployeeController with Details action to fetch employee by ID, set ViewData values, and render the Details view. Created Details.cshtml to display employee info, message, calculated temp, and branches list.

---
+++ MVC/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        ITIContext context = new ITIContext();
+
+        public IActionResult Details(int ID)
+        {
+            string msg = "Employee Message";
+            int temp = 50;
+
+            List<String> branches = new List<String>();
+            branches.Add("Branch 1");
+            branches.Add("Branch 2");
+
+            ViewData["Message"] = msg;
+            ViewData["Temp"] = temp;
+            ViewData["Branches"] = branches;
+
+            Employee employeeModel = context.Employee.FirstOrDefault(e => e.ID == ID);
+            return View("Details", employeeModel);
+        }
+    }
+}
