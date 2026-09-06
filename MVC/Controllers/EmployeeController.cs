4add4c38f64e31d1681c4fe3f5ff94f1c297dc29
moaaz-abdullah <moaaz200026@gmail.com>
2026-09-06 13:17:34 +0300

    Add Employee details ViewModel and DetailsVM view
    Added EmployeeDebtColorTempVM to encapsulate employee and department details, temperature, message, color, and branches. Implemented DetailsVM action in EmployeeController to use the new ViewModel and created DetailsVM.cshtml view. Updated using directives as needed.

--- MVC/Controllers/EmployeeController.cs
+++ MVC/Controllers/EmployeeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MVC.Models;
+using MVC.ViewModel;
 
 namespace MVC.Controllers
 {
@@ -26,6 +28,26 @@ namespace MVC.Controllers
 
             Employee employeeModel = context.Employee.FirstOrDefault(e => e.ID == ID);
             return View("Details", employeeModel);
+        }
+
+        public IActionResult DetailsVM(int ID)
+        {
+            List<String> branches = new List<String>();
+            branches.Add("Branch 1");
+            branches.Add("Branch 2");
+
+            Employee employeeModel = context.Employee.Include(e => e.Department).FirstOrDefault(e => e.ID == ID);
+
+            EmployeeDebtColorTempVM employeeViewModel = new EmployeeDebtColorTempVM();
+
+            employeeViewModel.EmpName = employeeModel.Name;
+            employeeViewModel.DeptName = employeeModel.Department.Name;
+            employeeViewModel.Temp = 12;
+            employeeViewModel.Message = "Message";
+            employeeViewModel.Color = "Red";
+            employeeViewModel.Branches = branches;
+
+            return View("DetailsVM", employeeViewModel);
         }
     }
 }
---
+++ MVC/ViewModel/EmployeeDebtColorTempVM.cs
@@ -0,0 +1,19 @@
+﻿namespace MVC.ViewModel
+{
+    public class EmployeeDebtColorTempVM
+    {
+        public string EmpName { get; set; }
+
+        public string DeptName { get; set; }
+        
+        public int Temp { get; set; }
+        
+        public string Message { get; set; }
+
+        public string Color{ get; set; }
+    
+        public List<string> Branches { get; set; }
+
+
+    }
+}
