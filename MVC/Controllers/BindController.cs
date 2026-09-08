7103515ec2ae2f3631bc9affa402085b01435de8
moaaz-abdullah <moaaz200026@gmail.com>
2026-09-08 18:14:52 +0300

    Add BindController with model binding action methods
    Introduced BindController in MVC.Controllers with three actions:
    - TestPrimitive for binding primitives and arrays
    - TestDic for binding a dictionary from query parameters
    - TestObj for binding a Department model object from query parameters

---
+++ MVC/Controllers/BindController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class BindController : Controller
+    {
+        // GET: /Bind/TestPrimitive?name=John&age=30&ID=123&color=red&color=blue
+        public IActionResult TestPrimitive(string name, int age, int ID, string[] color)
+        {
+            return Content($"{name} \t {age} \t {ID} \t {string.Join(", ", color)}");
+        }
+
+        // GET: /Bind/TestDic?name=John&age=30&ID=123
+        public IActionResult TestDic(Dictionary<string, string> dic)
+        {
+            return Content($"{dic["name"]} \t {dic["age"]} \t {dic["ID"]}");
+        }
+
+        // GET: /Bind/TestObj?ID=1&Name=HR&ManagerName=Alice
+        public IActionResult TestObj(Department department)
+        {
+            return Content($"{department.ID} \t {department.Name} \t {department.ManagerName}");
+        }
+    }
+}
