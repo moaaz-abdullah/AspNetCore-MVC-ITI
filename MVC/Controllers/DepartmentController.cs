2c734979173a7d20d3b76cdfb45695b24b49bb61
moaaz-abdullah <moaaz200026@gmail.com>
2026-09-09 14:00:04 +0300

    Add department creation form and save functionality
    Added Create and Save actions to DepartmentController for department creation. Updated Index.cshtml with a "Create New Department" button. Added Create.cshtml view for entering and submitting new department details.

--- MVC/Controllers/DepartmentController.cs
+++ MVC/Controllers/DepartmentController.cs
@@ -12,5 +12,21 @@ namespace MVC.Controllers
             List<Department> departments = context.Department.ToList();
             return View("Index", departments);
         }
+
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        public IActionResult Save(Department department)
+        {
+            if (department.Name != null && department.ManagerName != null)
+            {
+                context.Department.Add(department);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View("Create", department);
+        }
     }
 }
